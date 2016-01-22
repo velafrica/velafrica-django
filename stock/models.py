1d0e272176d112d9623683cdd87b44ad3001363f
platzhersh <chregi.glatthard@gmail.com>
2016-01-23 00:45:33 +0100

    add color to Category

--- stock/models.py
+++ stock/models.py
@@ -1,7 +1,7 @@
+from django.core.validators import RegexValidator
 from django.db import models
 from django_resized import ResizedImageField
 from simple_history.models import HistoricalRecords
-
 
 class Category(models.Model):
     """
@@ -10,6 +10,19 @@ class Category(models.Model):
     name = models.CharField(blank=False, null=False, max_length=255, verbose_name="Kategoriebezeichnung")
     description = models.TextField(blank=True, null=True, verbose_name="Beschreibung")
     image = ResizedImageField(size=[500, 500], upload_to='stock/categories/', blank=True, null=True, help_text='Product picture.')
+    color = models.CharField(
+        validators=[
+            RegexValidator(
+                regex="^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$", 
+                message="Must be a hexcode (e.g. #000 or #000000)",
+                code="invalid_hexcode"
+            )
+        ],
+        help_text="Colour code to use for this category (hex value, i.e. #000 or #000000)",
+        blank=True,
+        null=True,
+        max_length=7
+    )
     history = HistoricalRecords()
 
     def __unicode__(self):
@@ -22,7 +35,7 @@ class Category(models.Model):
 
 class Product(models.Model):
     """
-   	Represents a product category.
+    Represents a product category.
     """
     name = models.CharField(blank=False, null=False, max_length=255, verbose_name="Produktbezeichnung")
     sku = models.CharField(blank=False, null=False, max_length=255, verbose_name="SKU", unique=True)
