e30252f939f10da37c6a7ff7fdcfe24035da5308
platzhersh <chregi.glatthard@gmail.com>
2016-01-27 11:11:22 +0100

    remove bad postgres config

--- core/settings.py
+++ core/settings.py
@@ -85,14 +85,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    'prod': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mydatabase',
-        'USER': 'mydatabaseuser',
-        'PASSWORD': 'mypassword',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
     }
 }
 
