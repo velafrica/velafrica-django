d4c6fc34d30ecd307112937ab28a6b27306be906
platzhersh <chregi.glatthard@gmail.com>
2016-02-11 17:44:55 +0100

    ad more detailed dashboard

--- velafrica/core/views.py
+++ velafrica/core/views.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import timedelta, date
 from django.conf import settings
 from django.shortcuts import render_to_response
 from django.http import Http404
@@ -16,18 +16,52 @@ from velafrica.counter.models import Entry
 def home(request):
   return render_to_response('base.html')
 
+"""
+Counter
+"""
+
 def counter(request):
   velos_today = 0
+  velos_yesterday = 0
+  velos_thisweek = 0
+  velos_thismonth = 0
+  velos_thisyear = 0
   velos_total = 0
 
   entries = Entry.objects.all()
-  today = entries.first()
-  now = datetime.datetime.now()
+  latest = entries.first()
+  now = date.today()
+  first_day_of_week = now - timedelta(days=now.weekday())
 
-  if now.date() == today.date:
-      velos_today = today.amount
+  # check if latest entry was from today
+  if now == latest.date:
+      velos_today = latest.amount
 
   for entry in entries:
+    # sum up total
     velos_total += entry.amount
-  velos_total_str = str(velos_total)
-  return render_to_response('counter/index.html', { 'velos_total': velos_total_str, 'velos_today': velos_today })
+
+    # check if entry is from current year
+    if entry.date.year == now.year:
+      velos_thisyear += entry.amount
+
+      # check if entry is from current month
+      if entry.date.month == now.month:
+        velos_thismonth += entry.amount
+
+    # check if entry is from current week
+    if entry.date >= first_day_of_week:
+      velos_thisweek += entry.amount
+
+    # todo: get entry from yesterday
+    if entry.date == (now - timedelta(days=1)):
+      velos_yesterday += entry.amount
+
+  return render_to_response('counter/index.html', { 
+    'velos_total': velos_total,
+    'velos_thisyear': velos_thisyear,
+    'velos_thismonth': velos_thismonth,
+    'velos_thisweek': velos_thisweek,
+    'velos_yesterday': velos_yesterday,
+    'velos_today': velos_today
+    })
