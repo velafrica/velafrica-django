f334922b8fac883aca1bf7ede43a400a9d746adc
platzhersh <chregi.glatthard@gmail.com>
2016-03-10 16:33:12 +0100

    fix date format for export

--- velafrica/counter/admin.py
+++ velafrica/counter/admin.py
@@ -3,12 +3,18 @@ from velafrica.counter.models import Entry
 from velafrica.organisation.models import Organisation
 from import_export import resources
 from import_export.admin import ImportExportMixin
+from import_export.fields import Field
+from import_export.widgets import DateWidget
 from simple_history.admin import SimpleHistoryAdmin
 
 class EntryResource(resources.ModelResource):
     """
     Define the resource for counter entry.
     """
+    date = Field(
+        column_name='date',
+        attribute='date',
+        widget=DateWidget(format="%d.%m.%Y"))
 
     class Meta:
         model = Entry
