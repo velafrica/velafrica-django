5b67a6819666b91367a327c7d1661173028a07c3
platzhersh <chregi.glatthard@gmail.com>
2016-05-10 21:04:19 +0200

    adjust resource class

--- velafrica/sbbtracking/admin.py
+++ velafrica/sbbtracking/admin.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from simple_history.admin import SimpleHistoryAdmin
-from velafrica.sbbtracking.models import Tracking, TrackingEvent, TrackingEventType, EmailLog
+from velafrica.sbbtracking.models import Tracking, TrackingEvent, TrackingEventType, EmailLog, VeloType
 from import_export import resources
 from import_export.admin import ImportExportMixin
 
@@ -25,6 +25,9 @@ class EmailLogInline(admin.TabularInline):
     def has_add_permission(self, request):
         return False
 
+class VeloTypeAdmin(SimpleHistoryAdmin):
+    model = VeloType
+
 class TrackingResource(resources.ModelResource):
     """
     Define the Tracking resource for import / export.
@@ -33,7 +36,7 @@ class TrackingResource(resources.ModelResource):
     class Meta:
         model = Tracking
         import_id_fields = ('tracking_no',)
-        fields = ('first_name', 'last_name', 'email', 'tracking_no', 'number_of_velos', 'last_event', 'velo_type')
+        fields = ('first_name', 'last_name', 'email', 'tracking_no', 'number_of_velos', 'last_event__event_type__name', 'velo_type__name')
 
 class TrackingAdmin(ImportExportMixin, SimpleHistoryAdmin):
     resource_class = TrackingResource
@@ -92,5 +95,6 @@ class TrackingEventTypeAdmin(SimpleHistoryAdmin):
     list_display = ('name', 'description', 'send_email')
 
 
+admin.site.register(VeloType, VeloTypeAdmin)
 admin.site.register(Tracking, TrackingAdmin)
 admin.site.register(TrackingEventType, TrackingEventTypeAdmin)
