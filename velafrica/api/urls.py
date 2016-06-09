ac94d8ccc82fbae6eebedd31e36fa28013bf703a
platzhersh <chregi.glatthard@gmail.com>
2016-06-09 16:53:34 +0200

    remove deprecated patterns function from urls

--- velafrica/api/urls.py
+++ velafrica/api/urls.py
@@ -5,17 +5,17 @@ from velafrica.api import views
 
 
 
-stock = patterns('',
+stock = [
     url(r'^warehouses/?$', views.WarehouseList.as_view(), name="warehouses"),
     url(r'^warehouses/(?P<pk>[0-9]+)/?$', views.WarehouseDetail.as_view(), name='warehouses_detail'),
-)
+]
 
-organisation = patterns('',
+organisation = [
     url(r'^organisations/?$', views.OrganisationList.as_view(), name="organisations"),
     url(r'^organisations/(?P<pk>[0-9]+)/?$', views.OrganisationDetail.as_view(), name='organisations_detail'),
-)
+]
 
-tracking = patterns('',
+tracking = [
     url(r'^trackings/?$', views.TrackingList.as_view(), name="trackings"),
     url(r'^trackings/(?P<pk>[0-9]+)/?$', views.TrackingDetail.as_view(), name='trackings_detail'),
     url(r'^trackingevents/?$', views.TrackingEventList.as_view(), name="trackingevents"),
@@ -24,14 +24,14 @@ tracking = patterns('',
     url(r'^trackingeventtypes/(?P<pk>[0-9]+)/?$', views.TrackingEventTypeDetail.as_view(), name='trackingeventtypes_detail'),
     url(r'^velotypes/?$', views.VeloTypeList.as_view(), name="velotypes"),
     url(r'^velotypes/(?P<pk>[0-9]+)/?$', views.VeloTypeDetail.as_view(), name='velotypes_detail'),
-)
+]
 
 # where it all comes together
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', views.api_root),
     url(r'^organisation/', include(organisation, namespace="organisation")),
     url(r'^stock/', include(stock, namespace="stock")),
     url(r'^tracking/', include(tracking, namespace="tracking")),
-)
+]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
