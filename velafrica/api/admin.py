f07a24b987401b05fcd2ac46661b4b44612e3855
platzhersh <chregi.glatthard@gmail.com>
2016-07-18 14:56:25 +0200

    remove api user model

--- velafrica/api/admin.py
+++ velafrica/api/admin.py
@@ -1,5 +1,2 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from velafrica.api.models import ApiUser
-
-admin.site.register(ApiUser)
--- velafrica/api/models.py
+++ velafrica/api/models.py
@@ -1,16 +1,1 @@
 # -*- coding: utf-8 -*-
-from django.db import models
-from django.contrib.auth.models import User
-
-class ApiUser(models.Model):
-    """
-    ApiUser needs to be created for every user that should get permission to authenticate on the api.
-    """
-    user = models.ForeignKey(User)
-    active = models.BooleanField(default=False)
-
-    def __unicode__(self):
-        status = ""
-        if self.active:
-            status = " (inactive)"
-        return u"{}{}".format(self.user, status)
