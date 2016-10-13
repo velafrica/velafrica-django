5e0b7fb2d8a69600b9df421ae20fed54814e645a
HaRii <harald.ullmann@outlook.de>
2016-10-13 11:39:48 +0200

    changed models for tracking stations #16

--- velafrica/cms_plugins/models.py
+++ velafrica/cms_plugins/models.py
@@ -1,21 +1,46 @@
 from cms.models.pluginmodel import CMSPlugin
 from django.db import models
-from velafrica.sbbtracking.models import TrackingEventType, TrackingEvent
+from velafrica.sbbtracking.models import TrackingEventType
 
 
-class TrackingStationQuery(models.Model):
-    pass
+def get_event_types_as_choice():
+    tracking_event_types = TrackingEventType.objects.all()
+    ret_types = []
+    for event_type in tracking_event_types:
+        ret_types.append((event_type.id, event_type.name))
+
+    return ret_types
 
 
 class TrackingStation(CMSPlugin):
 
+    # had to do this, otherwise it will end up in a migration conflict/error
+    # read more here: https://www.django-cms.org/en/blog/2016/03/03/announcing-support-for-django-19/
+    # under *Important Issues Arise When Using Django 1.9
+    cmsplugin_ptr = models.OneToOneField(
+        CMSPlugin,
+        related_name='+',
+        parent_link=True
+    )
+
     def get_tracking_types(self):
-        tracking_types = TrackingEventType.objects.all()
-        ret_types = {}
-        for tt in tracking_types:
-            ret_types[tt.name] = TrackingEvent.objects.filter(event_type=tt).count()
+        return get_event_types_as_choice()
 
-        return ret_types
+    def copy_relations(self, old_instance):
+        for query in old_instance.queries.all():
+            query.pk = None
+            query.plugin = self
+            query.save()
 
 
-    loop_times = range(1, 7)
+class TrackingStationQuery(models.Model):
+
+    label = models.CharField(max_length=255, verbose_name="Label")
+    # TODO: find better solution!
+    #event_types = MultiSelectField(choices=lazy(get_event_types_as_choice, list)())
+    event_types = models.CharField(max_length=255, verbose_name="Event Typen")
+    query_order = models.IntegerField(verbose_name="Reihenfolge", default=0)
+    plugin = models.ForeignKey(
+        TrackingStation,
+        related_name="queries"
+    )
