ca4db694b97b8553c0264ff1800f8ecba138e5ca
HaRii <harald.ullmann@outlook.de>
2016-10-26 16:29:44 +0200

    refactored public_site views and addded logic for donations

--- velafrica/public_site/views.py
+++ velafrica/public_site/views.py
@@ -1,34 +1,51 @@
 # -*- coding: utf-8 -*-
 from django.core.urlresolvers import reverse
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 from paypal.standard.forms import PayPalPaymentsForm
 from velafrica.core.settings import PAYPAL_RECEIVER_MAIL
+from .forms import InvoiceForm
+from .models import DonationAmount
 
 def render_template(request):
     template_name = '/index'
-    template_context = {}
     if request.path != '/':
         template_name = request.path
 
-        # donation templates need more context data (like paypal form, invoice form and stuff)
-        if request.path == '/donation':
-            paypal_dict = {
-                "business": PAYPAL_RECEIVER_MAIL,
-                # "amount": amounts.first().amount,
-                "currency_code": "CHF",
-                "item_name": "Velafrica Donation",
-                "invoice": "unique-invoice-id",
-                "notify_url": "https://velafrica-staging-pr-21.herokuapp.com" + reverse('paypal-ipn'),
-                "return_url": '', # TODO: return url
-                "cancel_return": "",
-                "rm": "1",
-                "custom": "Upgrade all users!",  # Custom command to correlate to some function later (optional)
-            }
-            paypalform = PayPalPaymentsForm(initial=paypal_dict)
+    return render_to_response('public_site' + template_name + '.html', {
 
-            template_context.update({
-                'paypalform': paypalform
-            })
+    }, context_instance=RequestContext(request))
 
-    return render_to_response('public_site' + template_name + '.html', template_context, context_instance=RequestContext(request))
+
+def render_donation_template(request):
+    template_name = 'public_site/donation.html'
+    donation_amounts = DonationAmount.objects.filter(is_active=True).order_by('amount')
+    paypal_dict = {
+        "business": PAYPAL_RECEIVER_MAIL,
+        "amount": donation_amounts.first().amount,
+        "currency_code": "CHF",
+        "item_name": "Velafrica Donation",
+        "invoice": "unique-invoice-id",
+        "notify_url": "https://velafrica-staging-pr-21.herokuapp.com" + reverse('paypal-ipn'),
+        "return_url": '',  # TODO: return url
+        "cancel_return": "",
+        "rm": "1",
+        "custom": "Upgrade all users!",  # Custom command to correlate to some function later (optional)
+    }
+    paypalform = PayPalPaymentsForm(initial=paypal_dict)
+    invoiceform = InvoiceForm()
+    template_context = {
+        'amounts': donation_amounts,
+        'paypalform': paypalform,
+        'invoiceform': invoiceform
+    }
+
+    return render_to_response(template_name, template_context, context_instance=RequestContext(request))
+
+
+def order_invoice(request):
+    if request.method == 'POST':
+        form = InvoiceForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(form.cleaned_data['invoice_redirect_url'])
