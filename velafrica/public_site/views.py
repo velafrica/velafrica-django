3a7c1cae165f235c1f362e503243b9df2d5d1f71
HaRii <harald.ullmann@outlook.de>
2016-10-26 14:47:08 +0200

    refactored render_template function for more template_context

--- velafrica/public_site/views.py
+++ velafrica/public_site/views.py
@@ -1,12 +1,34 @@
 # -*- coding: utf-8 -*-
+from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from paypal.standard.forms import PayPalPaymentsForm
+from velafrica.core.settings import PAYPAL_RECEIVER_MAIL
 
 def render_template(request):
     template_name = '/index'
+    template_context = {}
     if request.path != '/':
-      template_name = request.path
+        template_name = request.path
 
-    return render_to_response('public_site' + template_name + '.html', {
+        # donation templates need more context data (like paypal form, invoice form and stuff)
+        if request.path == '/donation':
+            paypal_dict = {
+                "business": PAYPAL_RECEIVER_MAIL,
+                # "amount": amounts.first().amount,
+                "currency_code": "CHF",
+                "item_name": "Velafrica Donation",
+                "invoice": "unique-invoice-id",
+                "notify_url": "https://velafrica-staging-pr-21.herokuapp.com" + reverse('paypal-ipn'),
+                "return_url": '', # TODO: return url
+                "cancel_return": "",
+                "rm": "1",
+                "custom": "Upgrade all users!",  # Custom command to correlate to some function later (optional)
+            }
+            paypalform = PayPalPaymentsForm(initial=paypal_dict)
 
-    }, context_instance=RequestContext(request))
+            template_context.update({
+                'paypalform': paypalform
+            })
+
+    return render_to_response('public_site' + template_name + '.html', template_context, context_instance=RequestContext(request))
