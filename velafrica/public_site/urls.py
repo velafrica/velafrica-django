efb17e666f19ff865e9d160350645bf620435019
HaRii <harald.ullmann@outlook.de>
2016-10-28 12:50:12 +0200

    added basic map setup

--- velafrica/public_site/urls.py
+++ velafrica/public_site/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, url
 from .views import order_invoice
 from django.conf.urls import url
-from .views import render_template, render_donation_template, order_invoice, thank_you
+from .views import render_template, render_donation_template, render_map_template, order_invoice, thank_you
 
 donations = [
     url(r'^$', render_donation_template, name='home'),
@@ -9,7 +9,12 @@ donations = [
     url(r'^thank-you/$', thank_you, name='thank_you'),
 ]
 
+map = [
+    url(r'^$', render_map_template, name='home'),
+]
+
 urlpatterns = [
     url(r'^$', render_template, name='home'),
     url(r'^donation/', include(donations, namespace='donation')),
+    url(r'^map/', include(map, namespace='map')),
 ]
