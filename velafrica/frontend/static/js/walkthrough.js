3dee5dae443c0cfdcc13df35569c87ed2299ef06
HaRii <harald.ullmann@outlook.de>
2016-11-11 16:47:53 +0100

    added javascript

--- velafrica/frontend/static/js/walkthrough.js
+++ velafrica/frontend/static/js/walkthrough.js
@@ -17,11 +17,49 @@ if ($('#walkthrough').length) {
     $('input[name="optionsRadios"]').click(function () {
         if ($(this).attr("value") == "yes") {
             $('#id_collected_before_note').show();
-            $('#id_collected_before').val(true);
+            $('#id_collected_before').val('True');
         }
         if ($(this).attr("value") == "no") {
             $('#id_collected_before_note').hide();
-            $('#id_collected_before').val(false);
+            $('#id_collected_before').val('False');
+        }
+    });
+
+    $('input[name="dateRadios"]').click(function() {
+        if($(this).attr('value') == 'yes') {
+            $('#id_date_fixed').val('True');
+            $('#id_date').show();
+        }
+        if($(this).attr('value') == 'no') {
+            $('#id_date_fixed').val('False');
+            $('#id_date').hide();
+        }
+    });
+
+    $('input[name="storeRadios"]').click(function() {
+        if($(this).attr('value') == 'yes') {
+            $('#id_can_store').val('True');
+        }
+        if($(this).attr('value') == 'no') {
+            $('#id_can_store').val('False');
+        }
+    });
+
+    $('input[name="deliverRadios"]').click(function() {
+        if($(this).attr('value') == 'yes') {
+            $('#id_can_deliver').val('True');
+        }
+        if($(this).attr('value') == 'no') {
+            $('#id_can_deliver').val('False');
+        }
+    });
+
+    $('input[name="vaRadios"]').click(function() {
+        if($(this).attr('value') == 'yes') {
+            $('#id_velafrica_pickup').val('True');
+        }
+        if($(this).attr('value') == 'no') {
+            $('#id_velafrica_pickup').val('False');
         }
     });
 }
