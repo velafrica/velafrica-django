1eb4a74fe8f3997bbe0223e300371aa0472c7ae0
HaRii <harald.ullmann@outlook.de>
2016-11-11 14:21:02 +0100

    refactored walkthourh template to use modelform (the beginning)

--- velafrica/public_site/forms.py
+++ velafrica/public_site/forms.py
@@ -11,11 +11,35 @@ class InvoiceForm(forms.ModelForm):
 
 
 class WalkthroughRequestForm(forms.ModelForm):
+    last_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control input-lg',
+        'placeholder': 'Name*'
+    }))
+    first_name = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control input-lg',
+        'placeholder': 'Vorname*'
+    }))
+    phone = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control input-lg',
+        'placeholder': 'Telefonnummer*'
+    }))
+    email = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control input-lg',
+        'placeholder': 'E-Mail*'
+    }))
+    organizer_type = forms.ChoiceField(widget=forms.Select(attrs={
+        'class': 'form-control input-lg'
+    }), choices=WalkthroughRequest.PERSON_TYPE_CHOICES)
+    collected_before_note = forms.CharField(widget=forms.Textarea(attrs={
+        'class': 'form-control input-lg',
+        'placeholder': 'Wann und wo?',
+        'rows': '4'
+    }))
 
     class Meta:
         model = WalkthroughRequest
         fields = (
-            'first_name', 'last_name', 'phone', 'email', 'organizer_type', 'organizer_type_note',
+            'first_name', 'last_name', 'phone', 'email', 'organizer_type', 'collected_before', 'collected_before_note',
             'date_start', 'date_end', 'pickup_time_start', 'pickup_time_end', 'address', 'zip', 'address_note',
             'expected_velos', 'can_store', 'can_deliver', 'velafrica_pickup',
             'responsible_first_name', 'responsible_last_name', 'responsible_phone', 'responsible_email',
