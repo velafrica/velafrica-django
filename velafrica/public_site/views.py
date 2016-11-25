2ac90682cf7e11dcde3773c3bf5ee0506249a60f
HaRii <harald.ullmann@outlook.de>
2016-11-25 16:03:17 +0100

    added live data to tracking template context

--- velafrica/public_site/views.py
+++ velafrica/public_site/views.py
@@ -227,8 +227,13 @@ def render_personal_tracking(request, tracking_no):
 def render_tracking(request):
     template_name = 'public_site/tracking.html'
     template_context = {
-        'get_event_counts_url': reverse('api:public:tracking_live_data')
+        'get_event_counts_url': reverse('api:public:tracking_live_data'),
+
     }
+
+    data = Tracking.get_event_counts()
+    data['total'] = Tracking.get_tracked_velo_count()
+    template_context['data'] = data
 
     return render_to_response(template_name, template_context, context_instance=RequestContext(request))
 
