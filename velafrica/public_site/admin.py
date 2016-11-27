476b16ae3769bdb8c6f2085d9a6fc66a605f0087
HaRii <harald.ullmann@outlook.de>
2016-11-27 18:04:46 +0100

    added filter and visual feedback if a content object is filled

--- velafrica/public_site/admin.py
+++ velafrica/public_site/admin.py
@@ -1,4 +1,6 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import DonationAmount, InvoiceOrder, SbbTicketOrder, WalkthroughRequest, \
     Content, TeamMember, References, ContactRequest, Partner
 
@@ -8,10 +10,34 @@ class PartnerAdmin(admin.ModelAdmin):
     list_filter = ['country']
 
 
+class ContentAdminFilledFilter(admin.SimpleListFilter):
+    title = 'Befüllt'
+    parameter_name = 'filled'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('true', 'Ja'),
+            ('false', 'Nein'),
+        )
+
+    def queryset(self, request, queryset):
+
+        if self.value() == 'true':
+            return queryset.all().exclude(value__isnull=True).exclude(value__exact='')
+        elif self.value() == 'false':
+            return queryset.all().filter(value='')
+        else:
+            return queryset.all()
+
+
 class ContentAdmin(admin.ModelAdmin):
-    search_fields = ['path', 'key', 'description']
-    list_filter = ['language', 'path']
-    list_display = ['__unicode__', 'description']
+    search_fields = ['path', 'key', 'description', 'value']
+    list_filter = ('language', 'path', ContentAdminFilledFilter)
+    list_display = ['__unicode__', 'description', 'is_filled_column']
+
+    def is_filled_column(self, object):
+        source = 'yes' if object.value else 'no'
+        return format_html(u'<img src="/static/admin/img/icon-{}.svg" alt="{}"'.format(source, source))
 
 
 class TeamMemberAdmin(admin.ModelAdmin):
