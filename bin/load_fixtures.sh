b263a189d5ef06349ef7cea49a1e1adf256b9b7e
HaRii <harald.ullmann@outlook.de>
2016-11-28 15:14:28 +0100

    added loaddata for partner to load_fixtures.sh

--- bin/load_fixtures.sh
+++ bin/load_fixtures.sh
@@ -3,3 +3,4 @@
 python manage.py loaddata fixtures/public_site.json
 python manage.py loaddata fixtures/public_site.content.json
 python manage.py loaddata fixtures/public_site.teammember.json
+python manage.py loaddata fixtures/public_site.partner.json
