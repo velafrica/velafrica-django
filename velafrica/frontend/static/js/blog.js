107cf5d35ae80bda46c04e5f497c5f2feaeeae4d
Philipp Küng <git@philippkueng.ch>
2016-11-29 08:07:43 +0100

    makinng sure all the blog blocks have the same height #tp345

---
+++ velafrica/frontend/static/js/blog.js
@@ -0,0 +1,19 @@
+if ($('.blog-list').length) {
+  var sizePosts = function () {
+    var $posts = $('article.post-item .post_container');
+
+    // reset the hard-set height, so we can get the actual height the element needs
+    $posts.height('height', '');
+
+    var highestPostHeight = 0;
+    $posts.each(function() {
+      var postHeight = $(this).height();
+      if (postHeight > highestPostHeight) {
+        highestPostHeight = postHeight;
+      }
+    });
+
+    $posts.css('height', highestPostHeight + 30);
+  }
+  sizePosts();
+}
--- velafrica/frontend/static/js/main.js
+++ velafrica/frontend/static/js/main.js
@@ -26,6 +26,7 @@ $(document).ready(function () {
     require('newsletter');
     require('walkthrough');
     require('tracking');
+    require('blog');
 });
 
 
