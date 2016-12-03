9d5c6e5da044b747d15224c3b53a37ee026f2078
HaRii <harald.ullmann@outlook.de>
2016-12-03 19:59:55 +0100

    changed my-tracking url

--- velafrica/frontend/static/js/index.js
+++ velafrica/frontend/static/js/index.js
@@ -3,7 +3,7 @@ if ($('#tracking-code-form').length) {
     event.preventDefault();
     var trackingInput = $(this).find('input[class="tracking-code"]');
     if (trackingInput.val() != '') {
-      window.location.href = '/my-tracking/?tracking_no=' + trackingInput.val();
+      window.location.href = '/my-tracking/' + trackingInput.val();
     } else {
       //TODO: whatever velafrica wants to happen when no tracking code was entered
     }
