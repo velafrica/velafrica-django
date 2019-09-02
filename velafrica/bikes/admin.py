01a25f2c2dbd5584d60766b38de3c4ebd9b4a9bf
Tim Frey <tim@frey.uno>
2019-09-02 13:09:27 +0200

    Added Plot function for Bikes

--- velafrica/bikes/admin.py
+++ velafrica/bikes/admin.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 import datetime
 import io
+import os
 
 from django.conf.global_settings import MEDIA_ROOT
 from django.conf.urls import url
-from django.http import FileResponse, HttpResponse
+from django.core.exceptions import PermissionDenied
+from django.http import FileResponse, HttpResponse, StreamingHttpResponse
+from django.shortcuts import render
 from django.template.response import TemplateResponse
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import landscape, A4
@@ -20,102 +23,94 @@ from import_export.fields import Field
 from simple_history.admin import SimpleHistoryAdmin
 
 from velafrica.bikes.models import Bike
+from velafrica.core.settings import PROJECT_DIR
 
 
 class BikeResource(resources.ModelResource):
-
     class Meta:
         model = Bike
-
-# TODO: language-labels
-def draw_pdf_page(c, bike):
-    # Draw things on the PDF. Here's where the PDF generation happens.
-    # See the ReportLab documentation for the full list of functionality.
-    # W, H = landscape(A4)  # 841.9, 595.27
-    bikeD = model_to_dict(bike)
-
-    l = [
-        "number",
-        "type",
-        "brand",
-        "gearing",
-        "drivetrain",
-        "type_of_brake",
-        "brake",
-        "colour",
-        "size",
-        "suspension",
-        "extraordinary"
-    ]
-
-    # Header
-    c.setFont("Helvetica", 32)
-    c.drawString(x=50, y=510, text="A+ Bike")
-    c.drawImage("velafrica/frontend/static/img/velafrica_RGB.jpg", x=657.5, y=471, width=2466/15, height=1565/15)
-
-    # Bike
-    fontsize = 12
-    c.setFont("Helvetica", fontsize)
-
-    i = 0
-    for key in l:
-        if bikeD[key]:
-            c.drawString(50, 450 - 30*i - fontsize, key)
-            c.drawString(160, 450 - 30 * i - fontsize, str(bikeD[key]))
-            i += 1
-
-    # Image
-    if bike.image:
-        w = 480
-        h = bike.image.height / bike.image.width * w
-        c.drawImage("velafrica/" + bike.image.url, 321.9, 450-h, width=w, height=h)
-
-    # New Page
-    c.showPage()
 
 
 # TODO: show details fields as soon as A+ is selected
 class BikeAdmin(ImportExportMixin, DjangoObjectActions, SimpleHistoryAdmin):
+    labels = {key: Bike._meta.get_field(key).verbose_name for key in Bike.plotable}
+    fontsize = 12
+
     resource_class = BikeResource  # import export
 
-    list_display = ['number', 'type', 'brand', 'a_plus', 'container', 'warehouse']
-    search_fields = ['number', 'type', 'brand', 'a_plus', 'container', 'warehouse']
-    list_filter = ['type', ('date', DateRangeFilter), 'a_plus', 'container', 'warehouse']
+    list_display = ['id', 'type', 'brand', 'a_plus', 'for_sale', 'warehouse']
+    search_fields = ['id', 'type', 'brand', 'a_plus', 'for_sale', 'warehouse']
+    list_filter = ['for_sale', 'a_plus', 'warehouse', 'type', ('date', DateRangeFilter)]
     # change_actions = ('book_container',)
-    # readonly_fields = ['container_n_of_all', 'container_n_of_year', 'time_to_customer']
     # # inlines = [TrackingInline]
 
+    def for_sale(self, obj):
+        return obj.container is None
+
     actions = ["plot_to_pdf"]
+    readonly_fields = ['id']
 
     fieldsets = (
         (None, {
-            'fields': ('number', 'type', 'visum', 'date', 'warehouse')
-            }),
-        ('A+',
-             {
-                'fields': (
-                    'a_plus',
-                    'brand',
-                    'gearing',
-                    'drivetrain',
-                    'type_of_brake',
-                    'brake',
-                    'colour',
-                    'size',
-                    'suspension',
-                    'extraordinary',
-                    'image'
-                ),
-            }
+            'fields': ('id', 'number', 'type', 'visum', 'date', 'warehouse')
+        }),
+        ('A+',  # Details ?
+         {
+             'fields': (
+                 'a_plus',
+                 'brand',
+                 'gearing',
+                 'drivetrain',
+                 'type_of_brake',
+                 'brake',
+                 'colour',
+                 'size',
+                 'suspension',
+                 'extraordinary',
+                 'image'
+             ),
+         }
          ),
         ('Container',
-            {
-                'fields': [
-                    'container'
-                ]
-            }
+         {
+             'fields': [
+                 'container'
+             ]
+         }
          ),
     )
+
+    # TODO: language-labels
+    def draw_pdf_page(self, c, bike):
+        # Draw things on the PDF. Here's where the PDF generation happens.
+        # See the ReportLab documentation for the full list of functionality.
+        # W, H = landscape(A4)  # 841.9, 595.27
+
+        # Header
+        c.setFont("Helvetica", 32)
+        c.drawString(x=50, y=510, text="A+ Bike")
+        c.drawImage(
+            os.path.join(PROJECT_DIR, 'frontend', 'static', 'img/velafrica_RGB.jpg'),
+            x=657.5, y=471,
+            width=2466 / 15, height=1565 / 15
+        )
+
+        # Bike
+        c.setFont("Helvetica", self.fontsize)
+
+        i = 0
+        for key in Bike.plotable:
+            if bike.__getattribute__(key):  # not blank
+                c.drawString(50, 450 - 30 * i - self.fontsize, self.labels[key])
+                c.drawString(160, 450 - 30 * i - self.fontsize, str(bike.__getattribute__(key)))
+                i += 1
+
+        if bike.image:
+            w = 480
+            h = bike.image.height / bike.image.width * w
+            c.drawImage(bike.image.url, 321.9, 450 - h, width=w, height=h)
+
+        c.showPage()  # New Page
 
     def plot_to_pdf(self, request, queryset):
         # Create a file-like buffer to receive PDF data.
@@ -128,33 +123,17 @@ class BikeAdmin(ImportExportMixin, DjangoObjectActions, SimpleHistoryAdmin):
         )
 
         for b in queryset:
-            draw_pdf_page(p, b)
+            self.draw_pdf_page(p, b)
 
         # Close the PDF object cleanly, and we're done.
         p.save()
         buf.seek(0)
 
-        return HttpResponse(buf, content_type="application/pdf")
+        response = StreamingHttpResponse(buf, content_type="application/pdf")
+        response['Content-Disposition'] = "attachment;" + "filename={}_bikes.pdf".format(
+            datetime.datetime.now().strftime('%Y-%m-%d'))
 
-    plot_to_pdf.label = "Ausgewählte Velos als PDF drucken"
-
-    #
-    # def book_container(self, request, obj):
-    #     """
-    #     Admin action to book a container.
-    #     """
-    #     if obj:
-    #         result = obj.book()
-    #         print(result)
-    #         if result:
-    #             self.message_user(request, u"Container {} erfolgreich verbucht. {} von {} Trackings aktualisiert. ".format(obj.id, result[2], result[1]))
-    #         else:
-    #             self.message_user(request, "Container %s wurde bereits verbucht." % obj.id)
-    #     else:
-    #         self.message_user(request, "Not sure which Container to book." % obj.id)
-    #
-    # book_container.short_description = "Container Verbuchen"
-    # book_container.label = "Verbuchen"
+        return response
 
 
 class BikeInline(admin.TabularInline):
@@ -165,4 +144,3 @@ class BikeInline(admin.TabularInline):
 
 
 admin.site.register(Bike, BikeAdmin)
-
