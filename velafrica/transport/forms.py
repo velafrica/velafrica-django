c493b913081fe4b6638c1b9eb5ce53a1c065297c
Tim <timibern@gmail.com>
2019-12-31 14:26:34 +0100

    update admin form
    fieldset for transport request
    set auto generated fields readonly
    add autocompletion for Car

--- velafrica/transport/forms.py
+++ velafrica/transport/forms.py
@@ -2,21 +2,33 @@ from dal import autocomplete
 from django import forms
 
 from velafrica.stock.models import Warehouse
-from velafrica.transport.models import Ride, Driver
+from velafrica.transport.models import Ride, Driver, Car
 
 
 class RideForm(forms.ModelForm):
     from_warehouse = forms.ModelChoiceField(
         queryset=Warehouse.objects.all(),
-        widget=autocomplete.ModelSelect2(url='autocomplete:warehouse')
+        widget=autocomplete.ModelSelect2(url='autocomplete:warehouse'),
+        required=False,
+        label='von'
     )
     to_warehouse = forms.ModelChoiceField(
         queryset=Warehouse.objects.all(),
-        widget=autocomplete.ModelSelect2(url='autocomplete:warehouse')
+        widget=autocomplete.ModelSelect2(url='autocomplete:warehouse'),
+        required=False,
+        label='nach'
     )
     driver = forms.ModelChoiceField(
         queryset=Driver.objects.all(),
-        widget=autocomplete.ModelSelect2(url='autocomplete:driver')
+        widget=autocomplete.ModelSelect2(url='autocomplete:driver'),
+        required=False,
+        label='Fahrer'
+    )
+    car = forms.ModelChoiceField(
+        queryset=Car.objects.all(),
+        widget=autocomplete.ModelSelect2(url='autocomplete:car'),
+        required=False,
+        label='Fahrzeug'
     )
 
     class Meta:
