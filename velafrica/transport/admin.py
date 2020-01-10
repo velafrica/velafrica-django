fc7122b6a5b305efaa51d1eea7d2ec1bda5d1df0
Tim <timibern@gmail.com>
2020-01-10 16:59:29 +0100

    adjust export fields

--- velafrica/transport/admin.py
+++ velafrica/transport/admin.py
@@ -100,11 +100,11 @@ class RideResource(resources.ModelResource):
             'from_warehouse__name', 'to_warehouse__name', 'created_by', 'request_category__category_name',
             'planned_velos', 'request_comment', 'from_street_nr', 'from_zip_code', 'from_city', 'from_contact_name',
             'from_contact_phone', 'from_comment', 'to_street_nr', 'to_zip_code', 'to_city', 'to_contact_name',
-            'customer_company', 'customer_salutation', 'customer_firstname', 'customer_lastname',
-            'customer_email', 'customer_phone', 'customer_street_nr', 'customer_zip_code', 'customer_city',
-            'invoice_same_as_customer', 'charged', 'price', 'invoice_company_name', 'invoice_company_addition',
-            'invoice_street_nr', 'invoice_zip_code', 'invoice_city', 'invoice_commissioned', 'spare_parts', 'stocklist',
-            'distance'
+            'to_contact_phone', 'to_comment', 'customer_company', 'customer_salutation', 'customer_firstname',
+            'customer_lastname', 'customer_email', 'customer_phone', 'customer_street_nr', 'customer_zip_code',
+            'customer_city', 'invoice_same_as_customer', 'charged', 'invoice_purpose', 'price', 'cost_type',
+            'invoice_company_name', 'invoice_company_addition', 'invoice_street_nr', 'invoice_zip_code', 'invoice_city',
+            'invoice_commissioned', 'spare_parts', 'stocklist', 'distance'
         )
         fields = export_order
 
