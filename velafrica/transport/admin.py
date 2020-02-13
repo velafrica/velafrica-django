b8f6bcf07199a1c2c4a525a288dd2959f3919418
Tim <timibern@gmail.com>
2020-02-13 14:07:21 +0100

    change search fields for ride

--- velafrica/transport/admin.py
+++ velafrica/transport/admin.py
@@ -202,8 +202,9 @@ class RideAdmin(ImportExportMixin, SimpleHistoryAdmin):
         'id',
         'from_warehouse__name',
         'to_warehouse__name',
-        'driver__name',
-        'car__name'
+        'from_city',
+        'customer_lastname',
+        'from_contact_name',
     ]
 
     list_filter = [
