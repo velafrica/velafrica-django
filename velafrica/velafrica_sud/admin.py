f9313d61f4fcc6b31457b10c1abc6c5c970ece92
platzh1rsch <chregi.glatthard@gmail.com>
2020-05-14 15:28:32 +0200

    add shipment date to django admin

--- velafrica/velafrica_sud/admin.py
+++ velafrica/velafrica_sud/admin.py
@@ -41,9 +41,10 @@ class TrackingInline(admin.TabularInline):
                 if (field not in self.editable_fields):
                     fields.append(field)
         return fields
-    
+
     def has_add_permission(self, request):
         return False
+
 
 class ContainerResource(resources.ModelResource):
     """
@@ -52,31 +53,39 @@ class ContainerResource(resources.ModelResource):
 
     class Meta:
         model = Container
-        fields = ('container_no', 'container_n_of_year', 'organisation_from', 'organisation_from__name', 'partner_to', 'partner_to__organisation__name', 'velos_loaded', 'velos_unloaded', 'spare_parts', 'logistics', 'logistics__name', 'pickup_date', 'shipment_date', 'arrival_port_date', 'arrival_partner_date', 'velos_worth', 'spare_parts_worth', 'tools_worth', 'various_worth', 'seal_no', 'sgs_certified', 'notes')
+        fields = ('container_no', 'container_n_of_year', 'organisation_from', 'organisation_from__name', 'partner_to',
+                  'partner_to__organisation__name', 'velos_loaded', 'velos_unloaded', 'spare_parts', 'logistics',
+                  'logistics__name', 'pickup_date', 'shipment_date', 'arrival_port_date', 'arrival_partner_date',
+                  'velos_worth', 'spare_parts_worth', 'tools_worth', 'various_worth', 'seal_no', 'sgs_certified',
+                  'notes')
 
 
 class ContainerAdmin(ImportExportMixin, DjangoObjectActions, SimpleHistoryAdmin):
     resource_class = ContainerResource
-    list_display = ['pickup_date', 'container_no', 'organisation_from', 'warehouse_from', 'partner_to', 'velos_loaded', 'velos_unloaded', 'spare_parts', 'booked', 'notes']
+    list_display = ['pickup_date', 'container_no', 'organisation_from', 'warehouse_from', 'partner_to', 'velos_loaded',
+                    'velos_unloaded', 'spare_parts', 'booked', 'notes']
     search_fields = ['container_no', 'organisation_from__name', 'partner_to__organisation__name']
-    list_filter = ['pickup_date', ('pickup_date', DateRangeFilter), 'organisation_from', 'partner_to',]
+    list_filter = ['pickup_date', ('pickup_date', DateRangeFilter), 'organisation_from', 'partner_to', ]
     change_actions = ('print_bikes', 'display_bikes', 'book_container',)
     readonly_fields = ['container_n_of_all', 'container_n_of_year', 'time_to_customer']
-    #inlines = [TrackingInline]
+    # inlines = [TrackingInline]
     fieldsets = (
         (None, {
-            'fields': ('container_no', 'warehouse_from', 'organisation_from', 'partner_to', 'velos_loaded', 'velos_unloaded', 'spare_parts', 'stocklist', 'booked')
-            }),
+            'fields': (
+                'container_no', 'warehouse_from', 'organisation_from', 'partner_to', 'velos_loaded', 'velos_unloaded',
+                'spare_parts', 'stocklist', 'booked')
+        }),
         ('Transport', {
-            'fields': ('logistics', 'pickup_date', 'arrival_port_date', 'arrival_partner_date', 'time_to_customer')
-            }),
+            'fields': ('logistics', 'pickup_date', 'shipment_date', 'arrival_port_date', 'arrival_partner_date',
+                       'time_to_customer')
+        }),
         ('Worth', {
             'fields': ('velos_worth', 'spare_parts_worth', 'tools_worth', 'various_worth'),
             'classes': ('collapse',)
-            }),
+        }),
         ('Extra info', {
             'fields': ('seal_no', 'sgs_certified', 'notes')
-            })
+        })
     )
 
     def book_container(self, request, obj):
@@ -87,12 +96,14 @@ class ContainerAdmin(ImportExportMixin, DjangoObjectActions, SimpleHistoryAdmin)
             result = obj.book()
             print(result)
             if result:
-                self.message_user(request, u"Container {} erfolgreich verbucht. {} von {} Trackings aktualisiert. ".format(obj.id, result[2], result[1]))
+                self.message_user(request,
+                                  u"Container {} erfolgreich verbucht. {} von {} Trackings aktualisiert. ".format(
+                                      obj.id, result[2], result[1]))
             else:
                 self.message_user(request, "Container %s wurde bereits verbucht." % obj.id)
         else:
             self.message_user(request, "Not sure which Container to book." % obj.id)
-        
+
     book_container.short_description = "Container Verbuchen"
     book_container.label = "Verbuchen"
 
@@ -109,6 +120,7 @@ class ContainerAdmin(ImportExportMixin, DjangoObjectActions, SimpleHistoryAdmin)
                 partner_name=obj.partner_to.organisation.name
             )
         )
+
     print_bikes.label = "Print bikes"
 
     def display_bikes(self, request, obj):
@@ -118,6 +130,7 @@ class ContainerAdmin(ImportExportMixin, DjangoObjectActions, SimpleHistoryAdmin)
                 container_id=obj.id,
             )
         )
+
     display_bikes.label = "Show bikes"
 
 
@@ -138,18 +151,20 @@ class PartnerStaffInline(admin.TabularInline):
 class PartnerSudAdmin(SimpleHistoryAdmin):
     list_display = ['organisation', 'get_googlemaps_link', 'get_container_count', 'get_bicycle_count']
     search_fields = ['organisation__name', 'organisation__address__country__name']
-    readonly_fields = ['get_googlemaps_link', 'get_name', 'get_website', 'get_description', 'get_country', 'get_facebook', 'get_contact']
+    readonly_fields = ['get_googlemaps_link', 'get_name', 'get_website', 'get_description', 'get_country',
+                       'get_facebook', 'get_contact']
     inlines = [ContainerInline, PartnerStaffInline]
     fieldsets = (
         (None, {
-            'fields': ('organisation', 'get_name', 'get_contact', 'get_website', 'get_facebook', 'get_description', 'image')
-            }),
+            'fields': (
+                'organisation', 'get_name', 'get_contact', 'get_website', 'get_facebook', 'get_description', 'image')
+        }),
         ('Location', {
             'fields': ('get_googlemaps_link',)
-            }),
+        }),
         ('Organisation', {
-            'fields': ('legalform', 'org_type', 'partner_since', 'vocational_training', 'infrastructure' )
-            }),
+            'fields': ('legalform', 'org_type', 'partner_since', 'vocational_training', 'infrastructure')
+        }),
     )
 
     def get_googlemaps_link(self, obj):
@@ -163,7 +178,9 @@ class PartnerSudAdmin(SimpleHistoryAdmin):
                 ))
             else:
                 return ""
+
     get_googlemaps_link.short_description = 'Google Maps'
+
 
 class ReportStaffInline(admin.TabularInline):
     """
@@ -176,12 +193,20 @@ class ReportResource(resources.ModelResource):
     """
     Define the Conainter resource for import / export.
     """
-    economic_bicycles_turnover_USD = Field(readonly=True, attribute='economic_bicycles_turnover_USD', column_name='economic_bicycles_turnover_USD')
-    economic_turnover_total_USD = Field(readonly=True, attribute='economic_turnover_total_USD', column_name='economic_turnover_total_USD')
-    economic_spareparts_turnover_USD = Field(readonly=True, attribute='economic_spareparts_turnover_USD', column_name='economic_spareparts_turnover_USD')
-    economic_services_turnover_USD = Field(readonly=True, attribute='economic_services_turnover_USD', column_name='economic_services_turnover_USD')
-    economic_transport_costs_port_to_organisation_USD = Field(readonly=True, attribute='economic_transport_costs_port_to_organisation_USD', column_name='economic_transport_costs_port_to_organisation_USD')
-    communityproject_reinvest_profit_total_USD = Field(readonly=True, attribute='communityproject_reinvest_profit_total_USD', column_name='communityproject_reinvest_profit_total_USD')
+    economic_bicycles_turnover_USD = Field(readonly=True, attribute='economic_bicycles_turnover_USD',
+                                           column_name='economic_bicycles_turnover_USD')
+    economic_turnover_total_USD = Field(readonly=True, attribute='economic_turnover_total_USD',
+                                        column_name='economic_turnover_total_USD')
+    economic_spareparts_turnover_USD = Field(readonly=True, attribute='economic_spareparts_turnover_USD',
+                                             column_name='economic_spareparts_turnover_USD')
+    economic_services_turnover_USD = Field(readonly=True, attribute='economic_services_turnover_USD',
+                                           column_name='economic_services_turnover_USD')
+    economic_transport_costs_port_to_organisation_USD = Field(readonly=True,
+                                                              attribute='economic_transport_costs_port_to_organisation_USD',
+                                                              column_name='economic_transport_costs_port_to_organisation_USD')
+    communityproject_reinvest_profit_total_USD = Field(readonly=True,
+                                                       attribute='communityproject_reinvest_profit_total_USD',
+                                                       column_name='communityproject_reinvest_profit_total_USD')
 
     class Meta:
         model = Report
@@ -193,30 +218,33 @@ class ReportAdmin(ImportExportMixin, SimpleHistoryAdmin):
     search_fields = ['partner_sud']
     list_filter = ['partner_sud', 'creation']
     inlines = [ReportStaffInline]
-    readonly_fields = ['economic_bicycles_turnover_USD', 'economic_turnover_total_USD', 'economic_spareparts_turnover_USD', 'economic_services_turnover_USD', 'economic_transport_costs_port_to_organisation_USD', 'communityproject_reinvest_profit_total_USD']
+    readonly_fields = ['economic_bicycles_turnover_USD', 'economic_turnover_total_USD',
+                       'economic_spareparts_turnover_USD', 'economic_services_turnover_USD',
+                       'economic_transport_costs_port_to_organisation_USD',
+                       'communityproject_reinvest_profit_total_USD']
     fieldsets = (
         (None, {
             'fields': ('creation', 'partner_sud', 'currency', 'currency_rate')
-            }),
+        }),
         ('Employment', {
-            'classes': ('collapse', ),
+            'classes': ('collapse',),
             'fields': (
-                'employment_fulltime_men', 
-                'employment_fulltime_women', 
-                'employment_parttime_men', 
-                'employment_parttime_women', 
-                'employment_volunteer_men', 
-                'employment_volunteer_women', 
+                'employment_fulltime_men',
+                'employment_fulltime_women',
+                'employment_parttime_men',
+                'employment_parttime_women',
+                'employment_volunteer_men',
+                'employment_volunteer_women',
                 'employment_internship_men',
                 'employment_internship_women',
                 'employment_trainee_men',
                 'employment_trainee_women',
                 'employment_notes',
                 'employment_salary_calculation',
-                )
-            }),
+            )
+        }),
         ('Economic', {
-            'classes': ('collapse', ),
+            'classes': ('collapse',),
             'fields': (
                 'economic_bicycles_amount',
                 'economic_bicycles_turnover',
@@ -250,10 +278,10 @@ class ReportAdmin(ImportExportMixin, SimpleHistoryAdmin):
                 'economic_category8_pricerange',
                 'economic_payment_types',
                 'economic_notes'
-                )
-            }),
+            )
+        }),
         ('Marketing Channels', {
-            'classes': ('collapse', ),
+            'classes': ('collapse',),
             'fields': (
                 'marketing_customer_segments',
                 'marketing_customer_segments_other',
@@ -269,20 +297,20 @@ class ReportAdmin(ImportExportMixin, SimpleHistoryAdmin):
                 'marketing_channels_event_organisation',
                 'marketing_channels_event_attendance',
                 'marketing_channels_other',
-                )
-            }),
+            )
+        }),
         ('Selling Channels', {
-            'classes': ('collapse', ),
+            'classes': ('collapse',),
             'fields': (
                 'marketing_sales_shop',
                 'marketing_sales_outlets',
                 'marketing_sales_retail',
                 'marketing_sales_wholesale',
                 'marketing_sales_other',
-                )
-            }),
+            )
+        }),
         ('Vocational', {
-            'classes': ('collapse', ),
+            'classes': ('collapse',),
             'fields': (
                 'vocational_program_duration',
                 'vocational_program_girls',
@@ -299,19 +327,19 @@ class ReportAdmin(ImportExportMixin, SimpleHistoryAdmin):
                 'vocational_exstudents_familiybusiness',
                 'vocational_exstudents_energy',
                 'vocational_notes'
-                )
-            }),
+            )
+        }),
         ('Mobility', {
-            'classes': ('collapse', ),
+            'classes': ('collapse',),
             'fields': (
                 'mobilityprogram',
                 'mobilityprogram_people_benefitted',
                 'mobilityprogram_financial_support',
                 'mobilityprogram_notes'
-                )
-            }),
+            )
+        }),
         ('Community', {
-            'classes': ('collapse', ),
+            'classes': ('collapse',),
             'fields': (
                 'communityproject_reinvest_profit',
                 'communityproject_areas',
@@ -320,27 +348,28 @@ class ReportAdmin(ImportExportMixin, SimpleHistoryAdmin):
                 'communityproject_people_benefitted',
                 'communityproject_manager',
                 'communityproject_notes'
-                )
-            }),
+            )
+        }),
         ('Cooperation with Velafrica', {
-            'classes': ('collapse', ),
+            'classes': ('collapse',),
             'fields': (
                 'cooperation_quality_bicycles',
                 'cooperation_quality_spares',
                 'cooperation_quality_tools',
                 'cooperation_ordering_experience',
                 'cooperation_comments',
-                )
-            }),
+            )
+        }),
         ('Final comments', {
-            'classes': ('collapse', ),
+            'classes': ('collapse',),
             'fields': (
                 'final_biggest_success',
                 'final_future_challenges'
-                )
-            })
+            )
+        })
 
     )
+
 
 admin.site.register(Container, ContainerAdmin)
 admin.site.register(PartnerSud, PartnerSudAdmin)
